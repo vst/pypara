a177d29cb689769158b4e4ca664db050e2a7e71e
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2021-04-05 19:24:51 +0800

    chore: improve numbers module

--- pypara/commons/numbers.py
+++ pypara/commons/numbers.py
@@ -4,6 +4,7 @@ This module provides common numeric type definitions, constants and functions.
 
 __all__ = [
     "Amount",
+    "sign",
     "CENT",
     "DecimalLike",
     "HUNDRED",
@@ -15,10 +16,19 @@ __all__ = [
     "ZERO",
     "isum",
     "make_quantizer",
+    "Quantizer2",
+    "Quantizer4",
+    "Quantizer8",
+    "Quantizer12",
+    "quantize2",
+    "quantize4",
+    "quantize8",
+    "quantize12",
 ]
 
+import sys
 from decimal import Decimal
-from typing import Iterable, NewType, Optional, TypeVar, cast
+from typing import Callable, Iterable, NewType, Optional, TypeVar, cast
 
 #: Defines a new-type for non-negative (absolute, unsigned) :py:class:`decimal.Decimal` values as *amount*.
 #:
@@ -57,11 +67,73 @@ def make_quantizer(precision: int) -> Decimal:
     return Decimal(f"0.{''.join(['0' * precision])}")
 
 
+def make_quantize_func(quantizer: Decimal) -> Callable[[Decimal], Decimal]:
+    """
+    Creates a quantize function with the given quantizer.
+    """
+    return lambda x: x.quantize(quantizer)
+
+
 #: Defines the maximum precision of the monetary values and operations in this library.
 MaxPrecision: int = 12
 
 #: Defines the maximum precision quantifier.
 MaxPrecisionQuantizer: Decimal = make_quantizer(MaxPrecision)
+
+#: Defines a quantizer with 2 decimals.
+Quantizer2 = make_quantizer(2)
+
+#: Defines a quantizer with 4 decimals.
+Quantizer4 = make_quantizer(4)
+
+#: Defines a quantizer with 8 decimals.
+Quantizer8 = make_quantizer(8)
+
+#: Defines a quantizer with 12 decimals.
+Quantizer12 = make_quantizer(12)
+
+
+quantize2 = make_quantize_func(Quantizer2)
+quantize2.__doc__ = """
+Quantizes the given decimal (by 2 decimals).
+
+>>> quantize2(Decimal("0.005"))
+Decimal('0.00')
+>>> quantize2(Decimal("0.015"))
+Decimal('0.02')
+"""
+
+
+quantize4 = make_quantize_func(Quantizer4)
+quantize4.__doc__ = """
+Quantizes the given decimal (by 4 decimals).
+
+>>> quantize4(Decimal("0.00005"))
+Decimal('0.0000')
+>>> quantize4(Decimal("0.00015"))
+Decimal('0.0002')
+"""
+
+quantize8 = make_quantize_func(Quantizer8)
+quantize8.__doc__ = """
+Quantizes the given decimal (by 8 decimals).
+
+>>> quantize8(Decimal("0.000000005"))
+Decimal('0E-8')
+>>> quantize8(Decimal("0.000000015"))
+Decimal('2E-8')
+"""
+
+
+quantize12 = make_quantize_func(Quantizer12)
+quantize12.__doc__ = """
+Quantizes the given decimal (by 12 decimals).
+
+>>> quantize12(Decimal("0.0000000000005"))
+Decimal('0E-12')
+>>> quantize12(Decimal("0.0000000000015"))
+Decimal('2E-12')
+"""
 
 
 def isum(xs: Iterable[DecimalLike], start: Optional[DecimalLike] = None) -> DecimalLike:
@@ -86,3 +158,80 @@ def isum(xs: Iterable[DecimalLike], start: Optional[DecimalLike] = None) -> Deci
     Decimal('3')
     """
     return sum(xs, start or cast(DecimalLike, ZERO))
+
+
+def sign(x: Numeric) -> int:
+    """
+    Defines a sign predicate for numeric values (ints, floats and Decimals).
+
+    :param number: A number.
+    :return: The sign of the given number.
+
+    >>> sign(1)
+    1
+    >>> sign(0)
+    0
+    >>> sign(-0)
+    0
+    >>> sign(-1)
+    -1
+    >>> sign(Decimal("1"))
+    1
+    >>> sign(Decimal("0"))
+    0
+    >>> sign(-Decimal("0"))
+    0
+    >>> sign(Decimal("-1"))
+    -1
+    """
+    return 1 if x > 0 else -1 if x < 0 else 0
+
+
+def normalize(value: Decimal) -> Decimal:
+    """
+    Normalizes a given decimal value.
+
+    :param value: The decimal value to be normalized.
+    :return: The normalized decimal value.
+
+    >>> normalize(Decimal("0.00"))
+    Decimal('0')
+    """
+    return value.quantize(ONE) if value == value.to_integral() else value.normalize()
+
+
+def weirdiv(dividend: Optional[Decimal], divisor: Optional[Decimal]) -> Decimal:
+    """
+    Provides a division for weirdos.
+
+    Essentially, we are using this function at such call-sites where we don't have control over the incoming data, and
+    lazy enough to bother doing it there.
+
+    :param dividend: An optional dividend.
+    :param divisor: An optional divisor.
+    :return: A Decimal whether dividend and/or divisor are missing (0 in that case)
+
+    >>> weirdiv(None, None)
+    Decimal('0')
+    >>> weirdiv(None, Decimal(0))
+    Decimal('0')
+    >>> weirdiv(None, Decimal(1))
+    Decimal('0')
+    >>> weirdiv(Decimal(0), None)
+    Decimal('0')
+    >>> weirdiv(Decimal(1), None) > 10 ** 10
+    True
+    >>> weirdiv(Decimal(9), Decimal(3))
+    Decimal('3')
+    """
+    ## Check the dividend:
+    if dividend is None or dividend.is_zero():
+        return ZERO
+
+    ## Check the divisor:
+    if divisor is None or divisor.is_zero():
+        ## Wish I could return infinity!
+        return Decimal(sys.float_info.max).copy_sign(dividend)
+
+    ## Normal division:
+    return dividend / divisor
