524081c440251d9291e6595e4e960a7a68986e4a
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2023-06-09 08:31:43 +0800

    test: consolidate test configuration in pyproject.toml

--- default.nix
+++ default.nix
@@ -8,7 +8,22 @@
 let
   ## Our base Python:
   thisPython = pkgs.${python}.override {
-    packageOverrides = pself: psuper: { };
+    packageOverrides = pself: psuper: {
+      flake8-pyproject = pself.buildPythonPackage rec {
+        pname = "flake8_pyproject";
+        version = "1.2.3";
+        format = "wheel";
+        src = pself.fetchPypi {
+          inherit pname version format;
+          python = "py3";
+          sha256 = "sha256-Ykn+U1RSBa9edoN2RNyAtMEAN+c6Dl24f/Vi11+1vUo=";
+        };
+        propagatedBuildInputs = [
+          pself.flake8
+          pself.tomli
+        ];
+      };
+    };
   };
 
   ## Our project definition:
@@ -16,16 +31,16 @@ let
 
   ## Our package:
   thisPackage = thisPython.pkgs.buildPythonPackage {
-      pname = thisProject.name;
-      version = thisProject.version;
-      format = "pyproject";
+    pname = thisProject.name;
+    version = thisProject.version;
+    format = "pyproject";
 
-      src = ./.;
+    src = ./.;
 
-      propagatedBuildInputs = [
-        thisPython.pkgs.python-dateutil
-      ];
-    };
+    propagatedBuildInputs = [
+      thisPython.pkgs.python-dateutil
+    ];
+  };
 
   ## Our Python with production and development packages:
   thisPythonWithDeps = thisPython.withPackages (ps: with ps;
@@ -34,6 +49,7 @@ let
       ps.black
       ps.build
       ps.flake8
+      ps.flake8-pyproject
       ps.ipython
       ps.isort
       ps.mypy
